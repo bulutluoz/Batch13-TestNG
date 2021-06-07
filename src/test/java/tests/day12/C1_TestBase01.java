c67d5f886298feb41fadfecffb77e233c6926951
ahmet bulutluoz <abbulutluoz@gmail.com>
2021-06-07 22:21:05 +0300

    ders arasi

---
+++ src/test/java/tests/day12/C1_TestBase01.java
@@ -0,0 +1,14 @@
+package tests.day12;
+
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C1_TestBase01 extends TestBase {
+
+    @Test
+    public void test01(){
+
+        driver.get("https://www.youtube.com");
+
+    }
+}
---
+++ src/test/java/utilities/TestBaseClass.java
@@ -0,0 +1,29 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class TestBaseClass {
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+
+    @AfterClass
+    public void tearDown(){
+        driver.close();
+    }
+}
