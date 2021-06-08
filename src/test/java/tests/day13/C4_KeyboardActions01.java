8f8f8ea0d3dca8138cba93c5f86e7d5d4c652dd9
ahmet bulutluoz <abbulutluoz@gmail.com>
2021-06-08 23:18:54 +0300

    13.gun sonu

--- src/test/java/tests/day13/C4_KeyboardActions01.java
+++ src/test/java/tests/day13/C4_KeyboardActions01.java
@@ -1,5 +1,10 @@
 package tests.day13;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.TestBase;
 
@@ -8,12 +13,36 @@ public class C4_KeyboardActions01 extends TestBase {
     1- Bir Class olusturalim D14_KeyboardActions1
     2- https://www.amazon.com sayfasina gidelim
     3- Arama kutusuna actions method’larine kullanarak samsung A71 yazdirin ve Enter’a basarak arama yaptirin
-    4- basarili login olmadigini test edin
+    4- aramanin dogru yapildigini test edin
 
      */
 
     @Test
     public void test01(){
+
+        //2- https://www.amazon.com sayfasina gidelim
+        driver.get("https://www.amazon.com");
+
+        // 3- Arama kutusuna actions method’larine kullanarak samsung A71 yazdirin ve Enter’a basarak arama yaptirin
+
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+
+        Actions actions = new Actions(driver);
+
+        actions.
+                click(aramaKutusu).
+                sendKeys("samsung ").
+                keyDown(Keys.SHIFT).
+                sendKeys("a").
+                keyUp(Keys.SHIFT).
+                sendKeys("71").
+                sendKeys(Keys.ENTER).
+                perform();
+
+        String actualTitle=driver.getTitle();
+        String arananKelime= "samsung A71";
+
+        Assert.assertTrue(actualTitle.contains(arananKelime));
 
     }
 }
---
+++ src/test/java/tests/day13/C5_KeyboardActions02.java
@@ -0,0 +1,42 @@
+package tests.day13;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C5_KeyboardActions02 extends TestBase {
+    /*
+        1- Bir Class olusturalim D14_KeyboardActions2
+        2- https://html.com/tags/iframe/ sayfasina gidelim
+        3- video’yu gorecek kadar asagi inin
+        4- videoyu izlemek icin Play tusuna basin
+        5- videoyu calistirdiginizi test edin
+     */
+
+    @Test
+    public void test01(){
+        // 2- https://html.com/tags/iframe/ sayfasina gidelim
+        driver.get("https://html.com/tags/iframe/");
+
+        //3- video’yu gorecek kadar asagi inin
+        Actions actions=new Actions(driver);
+        actions.
+                sendKeys(Keys.PAGE_DOWN).
+                sendKeys(Keys.PAGE_DOWN).
+                perform();
+        //4- videoyu izlemek icin Play tusuna basin
+        WebElement iFrame=driver.findElement(By.xpath("//iframe[@class='lazy-loaded']"));
+
+        driver.switchTo().frame(iFrame);
+
+
+        driver.findElement(By.xpath("//button[@class='ytp-large-play-button ytp-button']")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='html5-video-player ytp-exp-bottom-control-flexbox ytp-title-enable-channel-logo ytp-embed ytp-embed-playlist ad-created playing-mode ytp-autohide']")).isDisplayed());
+
+    }
+}
